dd975c656184ac1f961934aa4512f94592032e75
Luana Faria Lane <luanasfff@gmail.com>
2019-11-15 05:51:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+ def each2_returning_new_values
+        i = 0;
+        new_vals = [];
+        while self[i]
+            new_vals[i] = yield self[i]
+            i += 1
+        end
+        new_vals
+    end
+end
 
