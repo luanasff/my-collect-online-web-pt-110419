f394895093befbb1664a85bb727b5e3d705b6c07
Luana Faria Lane <luanasfff@gmail.com>
2019-11-15 05:54:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+ def my_collect(collection)
+        i = 0;
+        new_collection = [];
+        while collection[i]
+            new_collection[i] = yield collection[i]
+            i += 1
+        end
+        new_collection
+    end
 
 
